ce62bb227ae2b4ec4ab33047b41e40b149f6c366
drincastX <drincast@gmail.com>
2017-12-14 17:13:03 -0500

    se agrega conexión a mongo mlab

--- src/Data/DataDB.ts
+++ src/Data/DataDB.ts
@@ -1,5 +1,25 @@
+import * as http from 'http';
 import * as mongoose from 'mongoose';
 const configApp = require('../configApp');
+
+const serverDB = http.createServer();
+const port = process.env.POST || 3000;
+const database = process.env.MONGO_URL || configApp.urlBDNube;
+
+// mongoose.connect(database, onDBConnect);
+// server.on('request', router);
+// server.on('listening', onListening);
+//
+// function onDBConnect(err, res){
+//     if(err) throw err;
+//     else{
+//         server.listen(port);
+//     }
+// }
+//
+// function onListening(){
+//     console.log(`server open on http://localhost:${port}`);
+// }
 
 export class DataDB{
   constructor(){
@@ -9,10 +29,26 @@ export class DataDB{
 
   init(){
     console.log('iniciando conexión ... en clase');
-    const MONGODB_CONNECTION: string = configApp.urlBD;
-    mongoose.connect(MONGODB_CONNECTION, {useMongoClient: true,});
+    console.log(configApp.urlBDNube);
+    //const MONGODB_CONNECTION: string = configApp.urlBD;
+    mongoose.connect(database, {useMongoClient: true,});
   }
 }
+
+
+
+// export class DataDB{
+//   constructor(){
+//     console.log('DAtaDB constructor ...');
+//     this.init();
+//   }
+//
+//   init(){
+//     console.log('iniciando conexión ... en clase');
+//     const MONGODB_CONNECTION: string = configApp.urlBD;
+//     mongoose.connect(MONGODB_CONNECTION, {useMongoClient: true,});
+//   }
+// }
 
 //const dataDB = new DataDB();
 //dataDB.init();
